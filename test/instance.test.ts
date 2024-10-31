262457ea7247799a9ab10a1a76e40804d2f4cfa8
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-10-31 20:30:20 +0700

    Operator instanceof - video 55

---
+++ test/instance.test.ts
@@ -0,0 +1,24 @@
+describe('Intance Of', () => {
+    class Employee {
+    }
+
+    class Manager {
+    }
+
+    const budi = new Employee()
+    const eko = new Manager()
+    
+    it('should have problem using typeof', () => {
+        
+        console.info(typeof budi)
+        console.info(typeof eko)
+    })
+
+    it('should support instanceof', () => {
+        expect(budi instanceof Employee).toBe(true)
+        expect(budi instanceof Manager).toBe(false)
+
+        expect(eko instanceof Employee).toBe(false)
+        expect(eko instanceof Manager).toBe(true)
+    })
+})
