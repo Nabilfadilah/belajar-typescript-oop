7a8ebfa45de0d788af78a0583d97ea2b625418b7
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-11-02 11:33:31 +0700

    Static - video 59

---
+++ test/static.test.ts
@@ -0,0 +1,30 @@
+describe('Static', () => {
+
+    class Configuration {
+        // properties
+        static NAME: string = "Belajar TypeScript OOP"
+        static VERSION: number = 1.0
+        static AUTHOR: string = "Mohammad Nabil Fadilah"
+    }
+
+    class MathUtil {
+        static sum(...values: number[]): number {
+            let total = 0
+            for (const value of values) {
+                total += value
+            }
+            return total
+        }
+    }
+
+    it('should support static method', () => {
+        console.info(MathUtil.sum(1, 2, 3, 4, 5))
+    })
+
+    it('should support', () => {
+        console.info(Configuration.NAME)
+        console.info(Configuration.VERSION)
+        console.info(Configuration.AUTHOR)
+    })
+
+})
